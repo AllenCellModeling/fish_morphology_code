49c7b579fec532e75ddd0eb64e4372cb4006c8d8
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-10-02 15:06:08 -0700

    rewrote read_and_contrast_image function

--- code/stretch.py
+++ code/stretch.py
@@ -90,27 +90,90 @@ def auto_contrast_fn(
     return out_array
 
 
-def read_and_contrast_image(filebase):
-    Cautos = []
-    Cmaxs = []
-    print(filebase)
-    C_im = AICSImage(filebase)
-    C_data = C_im.data
+def normalize_image_zero_one(im):
+    r"""
+    Normalize a Numpy array to have min zero and max one.
+    Args:
+        im (numpy.ndarray): data matrix
+    Returns:
+        (numpy.ndarray): normalized data matrix
+    """
+    im = im - np.min(im)
+    if np.max(im) > 0:
+        im = im / np.max(im)
+    return im
 
-    for k in range(5):
-        C = C_data[0, k, :, :, :]
-        print(C.shape)
-        Cmax = np.amax(C, axis=0)
-        print(Cmax.shape)
-        Cmax_n = Cmax / Cmax.max() * 255
-        print(Cmax_n.shape)
-        Cmax_n_auto = auto_contrast_fn(Cmax_n)
-        Cmaxs.append(Cmax)
-        Cautos.append(Cmax_n_auto)
 
-    for add in range(5, 10):
-        Cmaxs.append(C_data[0, add, 0, :, :])
-        Cautos.append((C_data[0, add, 0, :, :]))
+def float_to_uint(im, uint_dtype=np.uint8):
+    r"""
+    Convert an array of floats to unsigned ints, contrast stretrching so to the dynamic range of the output data type.
+    Args:
+        im (numpy.ndarray): data matrix
+        uint_dtype (numpy.dtype): numpy data type e.g. np.uint8
+    Returns:
+        (numpy.ndarray): integer data matrix
+    """
+    imax = np.iinfo(uint_dtype).max + 1  # eg imax = 256 for uint8
+    im = im * imax
+    im[im == imax] = imax - 1
+    im = np.asarray(im, uint_dtype)
+    return im
+
+
+def read_and_contrast_image(
+    image_path,
+    channels={
+        "bf": 0,
+        "488": 1,
+        "561": 2,
+        "638": 3,
+        "nuc": 4,
+        "seg488": 5,
+        "seg561": 6,
+        "seg638": 7,
+        "backmask": 8,
+        "cell": 9,
+    },
+    stretch_channels=["bf", "488", "561", "638", "nuc"],
+    verbose=True,
+):
+    r"""
+    Load an image from a file path, return two lists: max projects per channel,
+    and autocontrast versions of same.
+    Args:
+        image_path (str): location of input tiff image
+        channels (dict): {"channel_name":channel_index} map for input tiff
+        stretch_channels (list): list of channel names that to be contrast stretched
+        verbose (bool): print info while processing or not
+    Returns:
+        (Cmaxs, Cautos): tuple of two lists
+            - unadjusted maxprojects per channel
+            - stretched versions of same (for channels to be stretched, else unadjusted)
+    """
+    # print file path if desired
+    if verbose:
+        print(image_path)
+
+    # channel indices on which to apply auto_contrast_fn
+    raw_inds = [ind for channel, ind in channels.items() if channel in stretch_channels]
+
+    # read in all data for image
+    im = AICSImage(image_path)
+
+    # list of max projects for each channels
+    Cmaxs = [
+        float_to_uint(
+            normalize_image_zero_one(im.get_image_data("ZYX", T=0, C=c).max(axis=0)),
+            uint_dtype=np.uint8,
+        )
+        for c in sorted(channels.values())
+    ]
+
+    # auto contrast if image channel, original max proj if not
+    Cautos = [
+        auto_contrast_fn(Cdata, verbose=verbose) if c in raw_inds else Cdata
+        for c, Cdata in enumerate(Cmaxs)
+    ]
 
     return Cmaxs, Cautos
 
