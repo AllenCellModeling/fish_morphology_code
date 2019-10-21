9db369d332df0a721d6e2677a191ff26ce0c83d1
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-10-21 13:43:04 -0700

    merge cc files into code dir and rename dir

---
+++ fish_morphology_code/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Fish Morphology Code."""
+
+__author__ = "Rory Donovan-Maiye"
+__email__ = "rorydm@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.1.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
