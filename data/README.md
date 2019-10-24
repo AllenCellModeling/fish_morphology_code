b3416f05ca1351fc9d918e3e08a7d4d130fc800c
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-10-24 11:42:11 -0700

    files for uploading to quilt

---
+++ data/README.md
@@ -0,0 +1,18 @@
+# 2d segmented fields dataset
+
+This dataset contains 10-channel 2D tiffs of all the image fileds we use in our work.
+
+## Channels
+
+The descriptions of each channel are in `supporting_files/channel_defs.json`:
+
+ - the first five channels are maximun intesity projections of the original 3D images that were aquired.
+ - the other channels are segmentations of the max project channels or otherwise derived from them.
+
+## Columns
+
+The folllowing columns are included:
+
+ - `fov_id`: **(metadata)** noncanonical id for the field of view (fov)
+ - `original_fov_location`: **(metadata)** path on our local file system where raw microscopy data that generated the 2D images was stored
+ - `2D_tiff_path`: **(actual data)** path to the 10-channel 2D tiff included in this dataset
---
+++ data/distribute_seg_dataset.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from quilt3distribute import Dataset
+from quilt3distribute.validation import validate
+
+
+def distribute_seg_dataset(
+    csv_loc="data/input_segs_and_tiffs/raw_seg_013_014_images.csv",
+    col_name_map={
+        "fov_path": "original_fov_location",
+        "FOVId": "fov_id",
+        "seg_file_name": "2D_tiff_path",
+    },
+    dataset_name="2d_segmented_fields",
+    package_owner="rorydm",
+    s3_bucket="s3://allencell-internal-quilt",
+):
+
+    # read in original csv
+    df = pd.read_csv(csv_loc)
+
+    # rename some cols
+    df = df.rename(col_name_map, axis="columns")
+
+    # drop any cols with missing data
+    vds = validate(df, drop_on_error=True)
+    df = vds.data.reset_index(drop=True)
+
+    # create the dataset
+    ds = Dataset(
+        dataset=df,
+        name=dataset_name,
+        package_owner=package_owner,
+        readme_path="data/README.md",
+    )
+
+    # set data path cols, metadata cols, and extra files
+    ds.set_metadata_columns(["fov_id", "original_fov_location"])
+    ds.set_path_columns(["2D_tiff_path"])
+    ds.set_extra_files(["data/channel_defs.json"])
+
+    ds.distribute(s3_bucket)
