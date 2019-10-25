30926ff976e3da598ab70a1479159deb03daf361
Tanya Grancharova <tanyag@dev-aics-tgp-001.corp.alleninstitute.org>
2019-10-25 11:42:12 -0700

    cellprofiler and feature merge reorg

--- scripts/cellprofiler_output_processing/test_merge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python merge_cellprofiler_output.py \
-    "/allen/aics/gene-editing/FISH/2019/chaos/data/cp_testing/nuclei_filtering_by_centroid3" \
-    "napari_" \
-    "/allen/aics/gene-editing/FISH/2019/chaos/data/cp_testing/nuclei_filtering_by_centroid3/test_merged.csv"
