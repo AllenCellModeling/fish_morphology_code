eb35a482d37274c8e41fe17b95e6e88cfc17adaf
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-10-28 09:32:55 -0700

    test via pytest

--- fish_morphology_code/tests/test_stretch.py
+++ fish_morphology_code/tests/test_stretch.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-A simple example of a test file using a function.
-NOTE: All test file names must have one of the two forms.
-- `test_<XYY>.py`
-- '<XYZ>_test.py'
-
-Docs: https://docs.pytest.org/en/latest/
-      https://docs.pytest.org/en/latest/goodpractices.html#conventions-for-python-test-discovery
-"""
-
 from fish_morphology_code.bin.stretch import run
+from fish_morphology_code.bin.download_quilt_data import download_2D_segs
 
 
 def test_stretch():
     """test the auto contrast and single cell segmentation code"""
+
+    download_2D_segs(test=True)
     run(
         "quilt_data_test/metadata.csv",
         "quilt_data_test/supporting_files/channel_defs.json",
