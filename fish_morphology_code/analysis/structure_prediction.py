c645442f342632529beb8cd7388d7a3100532969
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-11-06 20:54:42 -0800

    struct predition code

--- fish_morphology_code/analysis/structure_prediction.py
+++ fish_morphology_code/analysis/structure_prediction.py
@@ -1,9 +1,14 @@
+import warnings
+
 import numpy as np
+import pandas as pd
 
 import sklearn
 from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.exceptions import UndefinedMetricWarning
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
 
 
@@ -115,3 +120,58 @@ def predict_on_all_datasets(trained_classifiers, dsets, verbose=False):
                         dset["X"]
                     )
     return out
+
+
+def add_human_classifier(
+    predictions, dsets, hc_col="mh_structure_org_score", hc_name="Melissa"
+):
+    for split, pred_splits in predictions.items():
+        for probe, pred_probe in pred_splits.items():
+            for feats, pred_feats in pred_probe.items():
+                predictions[split][probe][feats][hc_name] = {
+                    "pred": dsets[split][probe][feats]["adata"].obs[hc_col].values,
+                    "true": dsets[split][probe][feats]["y"],
+                }
+    return predictions
+
+
+def evaluate_predictions(predictions):
+    df_clf_report_ave = pd.DataFrame()
+    for split, pred_splits in predictions.items():
+        for probe, pred_probe in pred_splits.items():
+            for feats, pred_feats in pred_probe.items():
+                for clf_name, pred_clf in pred_feats.items():
+                    with warnings.catch_warnings():
+                        warnings.simplefilter("ignore", UndefinedMetricWarning)
+                        cr_dict = classification_report(
+                            pred_clf["true"], pred_clf["pred"], output_dict=True
+                        )
+                        cr_dict_ave = {
+                            k: v
+                            for k, v in cr_dict.items()
+                            if k in ["macro avg", "weighted avg"]
+                        }
+                        df_report_ave = (
+                            pd.DataFrame(cr_dict_ave)
+                            .transpose()
+                            .reset_index()
+                            .rename({"index": "Reduction"}, axis="columns")
+                        )
+                        df_report_ave["Split"] = split
+                        df_report_ave["Features"] = feats
+                        df_report_ave["Probe"] = probe
+                        df_report_ave["Classifier"] = clf_name
+                        df_clf_report_ave = df_clf_report_ave.append(
+                            df_report_ave, sort=True
+                        ).reset_index(drop=True)
+                        df_clf_report_ave["Classifier type"] = (
+                            df_clf_report_ave["Classifier"]
+                            .apply(lambda x: x.split()[0])
+                            .replace(
+                                {
+                                    "Logistic": "Logistic regression",
+                                    "Random": "Random Forest",
+                                }
+                            )
+                        )
+    return df_clf_report_ave
