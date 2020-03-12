290974483a7a65d61de236d2062566c07e5b94f8
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2020-03-12 15:17:10 -0700

    update to catch three outputs

--- fish_morphology_code/tests/test_load_data.py
+++ fish_morphology_code/tests/test_load_data.py
@@ -6,4 +6,4 @@ from fish_morphology_code.analysis.plots import load_data
 
 def test_load_data():
     "test loading the data"
-    df, df_tidy = load_data()
+    df, df_tidy, df_regression = load_data()
