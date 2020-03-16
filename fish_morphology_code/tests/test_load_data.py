659ce2dc1ce4793e2d888e48e11c25a829280d97
Rory Donovan-Maiye <donovanr@alumni.reed.edu>
2020-03-16 11:57:24 -0700

    add tests for recently add functions

--- fish_morphology_code/tests/test_load_data.py
+++ fish_morphology_code/tests/test_load_data.py
@@ -3,7 +3,7 @@
 
 import pytest
 
-from fish_morphology_code.analysis.data_ingestion import widen_df
+from fish_morphology_code.analysis.data_ingestion import widen_df, tidy_df
 
 from fish_morphology_code.analysis.plots import (
     make_small_dataset,
@@ -12,6 +12,10 @@ from fish_morphology_code.analysis.plots import (
     adata_manipulations,
     get_global_structure,
     group_human_scores,
+    make_regression_df,
+    clean_probe_names,
+    add_densities,
+    rename_dict,
 )
 
 
@@ -26,16 +30,19 @@ def test_load_main_feat_data(df_feats):
 
 
 def test_adata_manipulations(df_feats):
-    _ = adata_manipulations(df_feats)
+    adata = adata_manipulations(df_feats)
+    assert len(adata) > 0
 
 
 def test_make_small_dataset(df_feats):
     adata = adata_manipulations(df_feats)
-    _ = make_small_dataset(adata)
+    df_small = make_small_dataset(adata)
+    assert len(df_small) > 0
 
 
 def test_get_global_structure(df_feats):
-    _ = get_global_structure(use_cached=True)
+    df_gs = get_global_structure(use_cached=True)
+    assert len(df_gs) > 0
 
 
 def test_group_human_scores(df_feats):
@@ -44,7 +51,56 @@ def test_group_human_scores(df_feats):
     df_gs = get_global_structure(use_cached=True)
     df_small = df_small.merge(df_gs)
     df = widen_df(df_small)
-    _ = group_human_scores(df)
+    df = group_human_scores(df)
+    assert len(df) > 0
+
+
+def test_make_regression_df(df_feats):
+    adata = adata_manipulations(df_feats)
+    df_small = make_small_dataset(adata)
+    df_gs = get_global_structure(use_cached=True)
+    df_small = df_small.merge(df_gs)
+    df = widen_df(df_small)
+    df = group_human_scores(df)
+    df = df.rename(rename_dict, axis="columns")
+    df, df_regression_info = make_regression_df
+    assert len(df) > 0
+    assert len(df_regression_info) > 0
+
+
+def test_clean_probe_names(df_feats):
+    adata = adata_manipulations(df_feats)
+    df_small = make_small_dataset(adata)
+    df_gs = get_global_structure(use_cached=True)
+    df_small = df_small.merge(df_gs)
+    df = widen_df(df_small)
+    df = group_human_scores(df)
+    df = df.rename(rename_dict, axis="columns")
+    df, df_regression_info = make_regression_df
+    df_tidy = tidy_df(df)
+    df = df.rename(rename_dict, axis="columns")
+    df_tidy = df_tidy.rename(rename_dict, axis="columns")
+    df, df_tidy = clean_probe_names(df, df_tidy)
+    assert len(df) > 0
+    assert len(df_tidy) > 0
+
+
+def test_add_densities(df_feats):
+    adata = adata_manipulations(df_feats)
+    df_small = make_small_dataset(adata)
+    df_gs = get_global_structure(use_cached=True)
+    df_small = df_small.merge(df_gs)
+    df = widen_df(df_small)
+    df = group_human_scores(df)
+    df = df.rename(rename_dict, axis="columns")
+    df, df_regression_info = make_regression_df
+    df_tidy = tidy_df(df)
+    df = df.rename(rename_dict, axis="columns")
+    df_tidy = df_tidy.rename(rename_dict, axis="columns")
+    df, df_tidy = clean_probe_names(df, df_tidy)
+    df, df_tidy = add_densities(df, df_tidy)
+    assert len(df) > 0
+    assert len(df_tidy) > 0
 
 
 # def test_load_data():
