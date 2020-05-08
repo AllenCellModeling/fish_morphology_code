7c635d22d6164408b8f3a68337703ccfe449b9e2
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2020-05-08 15:55:35 -0700

    merge check

--- fish_morphology_code/tests/test_2d_nuclear_masks.py
+++ fish_morphology_code/tests/test_2d_nuclear_masks.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import quilt3
 from fish_morphology_code.bin.download_quilt_data import download_2D_nuclear_masks
 
 
 def test_download():
     "test downloading the data"
     download_2D_nuclear_masks(test=True)
+
+
+def test_merge():
+    """Check ability to merge on main df"""
+
+    # main df
+    p = quilt3.Package.browse(
+        "rorydm/manuscript_plots", "s3://allencell-internal-quilt"
+    )
+    df = p["data.csv"]()
+
+    # new df
+    p_new = quilt3.Package.browse(
+        "calystay/2d_nuclear_masks", "s3://allencell-internal-quilt"
+    )
+    df_new = (
+        p_new["metadata.csv"]()
+        .drop("Unnamed: 0", axis="columns")
+        .rename(
+            columns={
+                "fov_location": "FOV path",
+                "napariCell_ObjectNumber": "Cell number",
+            }
+        )
+    )
+
+    # merge
+    df_merged = df.merge(df_new, how="inner", on=["FOV path", "Cell number"])
+    assert len(df_merged) == len(df)
