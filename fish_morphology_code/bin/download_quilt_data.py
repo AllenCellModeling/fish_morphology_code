c6f51eb514766b77ee8dc99d87285246d3838bdb
Rory Donovan <donovanr@alumni.reed.edu>
2020-05-13 16:34:01 -0700

    fix conflict

--- fish_morphology_code/bin/download_quilt_data.py
+++ fish_morphology_code/bin/download_quilt_data.py
@@ -74,6 +74,24 @@ def download_2D_features(test=False):
         )
 
 
+def download_2D_nuclear_masks(test=False):
+    """download 2D nuclear mask images. if test=True, only download two randomly sampled images"""
+    if test:
+        download_quilt_data(
+            package="calystay/2d_nuclear_masks_test",
+            bucket="s3://allencell-internal-quilt",
+            data_save_loc="quilt_data_2d_nuclear_masks_test",
+            ignore_warnings=True,
+        )
+    else:
+        download_quilt_data(
+            package="calystay/2d_nuclear_masks",
+            bucket="s3://allencell-internal-quilt",
+            data_save_loc="quilt_data_2d_nuclear_masks",
+            ignore_warnings=True,
+        )
+
+
 def download_scrnaseq(test=False):
     """download scrnaseq data. if test=True, only download counts for ten random cells."""
     if test:
@@ -90,6 +108,16 @@ def download_scrnaseq(test=False):
             data_save_loc="quilt_data_scrnaseq",
             ignore_warnings=True,
         )
+
+
+def download_scrnaseq_raw(test=False):
+    """download scrnaseq raw data"""
+    download_quilt_data(
+        package="tanyasg/scrnaseq_data_raw",
+        bucket="s3://allencell-internal-quilt",
+        data_save_loc="quilt_data_scrnaseq_raw",
+        ignore_warnings=True,
+    )
 
 
 def download_ML_struct_scores(test=False):
@@ -110,6 +138,42 @@ def download_ML_struct_scores(test=False):
         )
 
 
+def download_nonstructure_2D_segs(test=False):
+    """download non-structure maxporject/seg data. if test=True, only download two randomly sampled images."""
+    if test:
+        download_quilt_data(
+            package="tanyasg/2d_nonstructure_fields_test",
+            bucket="s3://allencell-internal-quilt",
+            data_save_loc="quilt_data_nonstructure_test",
+            ignore_warnings=True,
+        )
+    else:
+        download_quilt_data(
+            package="tanyasg/2d_nonstructure_fields",
+            bucket="s3://allencell-internal-quilt",
+            data_save_loc="quilt_data_nonstructure",
+            ignore_warnings=True,
+        )
+
+
+def download_2D_nonstructure_features(test=False):
+    """download non-structure features. if test=True, only download features for two randomly sampled images."""
+    if test:
+        download_quilt_data(
+            package="tanyasg/2d_nonstructure_single_cell_features_test",
+            bucket="s3://allencell-internal-quilt",
+            data_save_loc="quilt_nonstructure_features_test",
+            ignore_warnings=True,
+        )
+    else:
+        download_quilt_data(
+            package="tanyasg/2d_nonstructure_single_cell_features",
+            bucket="s3://allencell-internal-quilt",
+            data_save_loc="quilt_nonstructure_features",
+            ignore_warnings=True,
+        )
+
+
 def main_segs():
     fire.Fire(download_2D_segs)
 
@@ -126,5 +190,17 @@ def main_scrnaseq():
     fire.Fire(download_scrnaseq)
 
 
+def main_scrnaseq_raw():
+    fire.Fire(download_scrnaseq_raw)
+
+
 def main_MLstruct():
     fire.Fire(download_ML_struct_scores)
+
+
+def main_nonstructure_segs():
+    fire.Fire(download_nonstructure_2D_segs)
+
+
+def main_nonstructure_features():
+    fire.Fire(download_2D_nonstructure_features)
