ed5b94468e494e202d5a6298bf0811bde0d84f6e
Rory Donovan <donovanr@alumni.reed.edu>
2020-05-13 17:43:25 -0700

    fix ml test

--- fish_morphology_code/tests/test_features.py
+++ fish_morphology_code/tests/test_features.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from fish_morphology_code.bin.download_quilt_data import download_2D_features, download_ML_struct_scores
+from fish_morphology_code.bin.download_quilt_data import download_2D_features
 
 
 def test_download():
     "test downloading the data"
     download_2D_features(test=True)
-
-def test_ml_scores_download()
-	"test downloading the data"
-    download_ML_struct_scores(test=True)
