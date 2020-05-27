4bbfed4ab307b920e4c7ba01848cd605ba5d6da0
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2020-05-27 16:20:04 -0700

    Data/go public (#149)
    * add public push
    
    * function for updating a ds readme quickly
    
    * fn to update all quilt readmes to github versions
    
    * add public bucked profile
    
    * use boto3 to set aws creds
    
    * keep internal and external connections open together

--- fish_morphology_code/bin/consolidate_datasets.py
+++ fish_morphology_code/bin/consolidate_datasets.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
 import subprocess
+import boto3
 import quilt3
 import fire
+
+
+def update_quilt_readme(
+    pkg_name="calystay/probe_localization",
+    s3_bucket="s3://allencell-internal-quilt",
+    new_readme_loc=Path("data/probe_localization/README.md"),
+):
+    """Quickly update a package's README without ahving to run all the distribute code."""
+    p = quilt3.Package.browse(pkg_name, s3_bucket)
+    p.set("README.md", new_readme_loc)
+    p.push(pkg_name, s3_bucket)
+
+
+README_MAP = {
+    "rorydm/2d_autocontrasted_fields_and_single_cells": "data/2d_autocontrasted_fields_and_single_cells/README.md",
+    "tanyasg/2d_autocontrasted_single_cell_features": "data/structure_cellprofiler_features/README.md",
+    "tanyasg/2d_nonstructure_fields": "data/nonstructure_fields/README.md",
+    "tanyasg/2d_nonstructure_single_cell_features": "data/nonstructure_features/README.md",
+    "calystay/2d_nuclear_masks": "data/nuclear_masks/README.md",
+    "rorydm/2d_segmented_fields": "data/2d_segmented_fields/README.md",
+    "calystay/3d_actn2_segmentation": "data/actn2_3d_seg/README.md",
+    "matheus/assay_dev_fish_analysis": "fish_morphology_code/processing/structure_organization/tools/assay-dev-fish.md",
+    "rorydm/manuscript_plots": "data/plots_dataset/README.md",
+    "calystay/probe_localization": "data/probe_localization/README.md",
+    "calystay/probe_structure_classifier": "data/probe_loc_struc_classifier/README.md",
+    "tanyasg/scrnaseq_data": "data/scrnaseq_counts/README.md",
+    "tanyasg/scrnaseq_raw": "data/scrnaseq_raw/README.md",
+}
+
+
+def update_all_quilt_readmes():
+    for k, v in README_MAP.items():
+        update_quilt_readme(pkg_name=k, new_readme_loc=v)
 
 
 def make_pkg_maps(
@@ -30,7 +65,7 @@ def make_pkg_maps(
 ):
 
     # grab a list of packages that actually exist internally
-    pkg_list = list(quilt3.list_packages("s3://allencell-internal-quilt"))
+    pkg_list = list(quilt3.list_packages(internal_s3_url))
 
     # create dictionary for original fully qualified name to simple name
     pkg_map = {p: p.split("/")[-1] for p in all_pkgs}
@@ -62,10 +97,14 @@ def make_pkg_maps(
 def aggregate_and_push(
     pkg_map,
     source_S3_url="s3://allencell-internal-quilt",
-    dest_S3_url="s3://allencell-internal-quilt",
-    dest_pkg_name="aics/cardio_diff_manuscript",
-    message="FISH data consolidation",
+    dest_S3_url="s3://allencell",
+    dest_pkg_name="aics/integrated_transcriptomics_structural_organization_hipsc_cm",
+    message="Public data set",
+    public=False,
 ):
+
+    internal = boto3.session.Session(profile_name="default")  # noqa: F841
+
     # real data
     q = quilt3.Package()
     q.set("README.md", "../../data/README.md")
@@ -84,6 +123,10 @@ def aggregate_and_push(
     )
     label = f"{message}. git commit hash of fish_morphology_code = {git_commit_hash}."
 
+    # set profile to public bucket access if pushing public
+    if public:
+        external = boto3.session.Session(profile_name="allencell")  # noqa: F841
+
     q.push(dest_pkg_name, dest_S3_url, message=label)
 
 
@@ -95,8 +138,8 @@ def agg_push_test():
         PKG_MAP_TEST,
         source_S3_url="s3://allencell-internal-quilt",
         dest_S3_url="s3://allencell-internal-quilt",
-        dest_pkg_name="aics/cardio_diff_manuscript_test",
-        message="FISH test data consolidation",
+        dest_pkg_name="aics/integrated_transcriptomics_structural_organization_hipsc_cm_test",
+        message="Public data set",
     )
 
 
@@ -105,8 +148,19 @@ def agg_push_full():
         PKG_MAP,
         source_S3_url="s3://allencell-internal-quilt",
         dest_S3_url="s3://allencell-internal-quilt",
-        dest_pkg_name="aics/cardio_diff_manuscript",
-        message="FISH data consolidation",
+        dest_pkg_name="aics/integrated_transcriptomics_structural_organization_hipsc_cm",
+        message="Public data set",
+    )
+
+
+def agg_push_public():
+    aggregate_and_push(
+        PKG_MAP,
+        source_S3_url="s3://allencell-internal-quilt",
+        dest_S3_url="s3://allencell",
+        dest_pkg_name="aics/integrated_transcriptomics_structural_organization_hipsc_cm",
+        message="Public data set",
+        public=True,
     )
 
 
